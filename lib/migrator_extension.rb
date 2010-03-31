e0353fce78a2d4cf2b44bfb9b91a87aeed42dcc0
smn <smn@sens3.com>
2010-03-31 15:33:06 -0400

    reloading column definitions before update_columns

--- lib/migrator_extension.rb
+++ lib/migrator_extension.rb
@@ -8,9 +8,11 @@ module MigratorExtension
   module InstanceMethods
     def migrate_with_deleted_table_update(*args)
       migrate_without_deleted_table_update(*args)
+      # reloading column definitions, was having issues with Post.columns not reflecting changes
+      ActiveRecord::Base.reset_column_information_and_inheritable_attributes_for_all_subclasses
       ActiveRecord::Acts::SoftDeletable::Live::InstanceMethods.included_in_classes.each do |klass|
         klass::Deleted.update_columns
-        puts "Updating columns of #{klass.name}::Deleted"
+        puts "Updated columns of #{klass.name}::Deleted"
       end
     end
   end
